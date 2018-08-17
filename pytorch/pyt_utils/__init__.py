6351ff15962a7d857ed756905e4541eb364175bb
Matt Phillips <matt.phillips@kitware.com>
2018-08-17 16:46:22 -0400

    Added models.py

---
+++ pytorch/pyt_utils/__init__.py
@@ -0,0 +1,5 @@
+from pyt_utils import encoder
+from pyt_utils import models
+
+__version__ = "0.0.1"
+
--- pytorch/setup.py
+++ pytorch/setup.py
@@ -2,18 +2,18 @@ from setuptools import setup, find_packages
 from distutils.util import convert_path
 
 version = None
-with open("utils/__init__.py") as fp:
+with open("pyt_utils/__init__.py") as fp:
     for line in fp:
         if line.startswith("__version__"):
             N = len(line)
             version = line[ line.index("\"")+1 : N-line[::-1].index("\"")-1 ]
             break
 
-PROJECT_DIRS = ["utils", ]
+PROJECT_DIRS = ["pyt_utils", ]
 
 if __name__ == '__main__':
     setup(setup_fpath=__file__,
-        name="pytorch_utils",
+        name="pyt_utils",
         author="Matt Phillips, Kitware",
         author_email="matt.phillips@kitware.com",
         version=version,
