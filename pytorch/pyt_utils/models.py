510283405e8c1ad8d49d3592d3e47abdc74066fd
Matt Phillips <matt.phillips@kitware.com>
2018-12-15 22:00:13 -0500

    Added alexnet_pret

--- pytorch/pyt_utils/models.py
+++ pytorch/pyt_utils/models.py
@@ -12,6 +12,9 @@ model_urls = {
     'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
 }
 
+#### TODO ####
+# Obviously should not be two different AlexNet versions! 
+
 class alexnet_wrapper(tv.models.AlexNet):
     def __init__(self, feats_only=False, load_pretrained=True, num_classes=1000,
             **kwargs):
@@ -61,6 +64,32 @@ class alexnet_wrapper(tv.models.AlexNet):
         x = self._get_features(x)
         if not self.feats_only:
             x = self.classifier_2(x)
+        return x
+
+from torchvision.models.alexnet import model_urls as alexnet_model_urls
+class alexnet_pret(tv.models.AlexNet):
+    def __init__(self, feats_only=False, load_pretrained=True, **kwargs):
+        super().__init__(**kwargs)
+        self._feats_only = feats_only
+        print("Features only: ", self._feats_only)
+        if load_pretrained:
+            print("Loading pretrained model")
+            self.load_state_dict(torch.utils.model_zoo.load_url(\
+                    alexnet_model_urls["alexnet"]))
+
+    def forward(self, x):
+        if self._feats_only:
+            return self._get_linear2(x)
+        else:
+            return super().forward(x)
+
+    def get_features(self, x):
+        return self._get_linear2(x)
+
+    def _get_linear2(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), 256 * 6 * 6)
+        x = self.classifier[:5](x)
         return x
 
 
