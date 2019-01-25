4588cca0cd5a6c9d066392ebdc508b4362ef72b6
matthew.phillips <matthew.phillips@lifeomic.com>
2019-01-25 13:46:32 -0500

    Added utilities for writing arguments, command lines, fixed bug in write_training_results

--- general/utils.py
+++ general/utils.py
@@ -3,10 +3,12 @@ Platform independent utilities
 """
 
 import csv
+import fnmatch
 import numpy as np
 import os
 import pathlib
 import shutil
+import sys
 
 from collections import OrderedDict
 
@@ -18,6 +20,27 @@ g_delete_me_txt = "delete_me.txt"
 g_session_dir_stub = "session_%02d"
 g_session_log = "session.log"
 
+
+# Copies the code (*.py, *.txt) files from the project repo as well as this
+# ml_utils repo into a session_dir subdirectory
+# Inputs:
+#   project_dir: The directory containing the python code repo
+#   session_dir: The training session directory
+def copy_code(project_dir, session_dir):
+    project_dir = os.path.abspath(project_dir)
+    project_name = os.path.basename(project_dir)
+    path_to_ml_utils = os.path.dirname(os.path.dirname(os.path.abspath(\
+            __file__)))
+    mlu_name = os.path.basename(path_to_ml_utils)
+    repos_dir = pj(session_dir, "repos")
+    if pe(repos_dir):
+        shutil.rmtree(repos_dir)
+    pdir = pj(repos_dir, project_name)
+    mludir = pj(repos_dir, mlu_name)
+    shutil.copytree(project_dir, pdir, ignore=include_patterns("*.py",
+        "*.txt"))
+    shutil.copytree(path_to_ml_utils, mludir,
+        ignore=include_patterns("*.py", "*.txt"))
 
 # WARNING if you call this funtion, you had better also call retain_session_dir
 # as well at the suitable time or else the session that was created  will get 
@@ -34,9 +57,73 @@ def create_session_dir(output_supdir, dir_stub=g_session_dir_stub):
     pathlib.Path( pj(stub % (ct), g_delete_me_txt) ).touch()
     return stub % (ct)
 
+def get_project_dir(project_name, file_path):
+    old_fp = os.path.abspath(file_path)
+    while os.path.basename(os.path.abspath(file_path)) != project_name:
+        file_path = os.path.dirname(file_path)
+        if file_path == old_fp:
+            raise RuntimeError("No project directory %s found in path %s" \
+                    % (project_name, file_path))
+        old_fp = file_path
+    return os.path.abspath(file_path)
+
+# Copied directly from https://stackoverflow.com/questions/35155382/copying      # -specific-files-to-a-new-folder-while-maintaining-the-original-subdirect
+def include_patterns(*patterns):
+    """ Function that can be used as shutil.copytree() ignore parameter that
+    determines which files *not* to ignore, the inverse of "normal" usage.
+
+    This is a factory function that creates a function which can be used as a
+    callable for copytree()'s ignore argument, *not* ignoring files that match
+    any of the glob-style patterns provided.
+
+    ‛patterns’ are a sequence of pattern strings used to identify the files to
+    include when copying the directory tree.
+
+    Example usage:
+
+        copytree(src_directory, dst_directory,
+                 ignore=include_patterns('*.sldasm', '*.sldprt'))
+    """
+    def _ignore_patterns(path, all_names):
+        # Determine names which match one or more patterns (that shouldn't be
+        # ignored).
+        keep = (name for pattern in patterns
+                        for name in fnmatch.filter(all_names, pattern))
+        # Ignore file names which *didn't* match any of the patterns given that
+        # aren't directory names.
+        dir_names = (name for name in all_names if os.path.isdir(pj(path,
+            name)))
+        return set(all_names) - set(keep) - set(dir_names)
+
+    return _ignore_patterns
+
+# This deletes the 'delete_me.txt' file which would otherwise signify that
+# this directory should be overwritten on the next training run.  Paired with
+# create_session_dir
 def retain_session_dir(session_dir):
     if pe(pj(session_dir, g_delete_me_txt)):
         os.remove( pj(session_dir, g_delete_me_txt) )
+
+# Appends the command-line arguments to the session log
+# Inputs:
+#   session_dir: The directory containing the session log.  It will be 
+#       created if it doesn't exist.
+def write_arguments(session_dir):
+    s = "python %s" % sys.argv[0]
+    for arg in sys.argv[1:]:
+        s += " %s" % arg
+    with open(pj(session_dir, g_session_log), "a") as fp:
+        fp.write("Command line:\n")
+        fp.write("%s\n\n" % s)
+
+# Appends the input parameters to the session log
+# Inputs:
+#   cfg: A dict containing all of the parameters.  Must contain an entry
+#       "session_dir" with a valid path.
+def write_parameters(cfg):
+    with open(pj(cfg["session_dir"], g_session_log), "a") as fp:
+        for k,v in cfg.items():
+            fp.write("%s: %s\n" % (k, repr(v)))
 
 # Inputs:
 #   results_dict: A dictionary of core training metrics, see code for required
@@ -48,10 +135,9 @@ def write_training_results(results_dict, cfg, project_dir, trainer_name):
     if not pe(project_dir):
         raise RuntimeError("project_dir %s should already exist"%(project_dir))
     results_keys = results_dict.keys()
-    if any([s not in results_keys for s in ["model", "loss", "accuracy",
-        "dataset"]]):
+    if any([s not in results_keys for s in ["loss", "accuracy"]]):
         raise RuntimeError("Argument results_dict must have standard keys: " \
-                "model, loss, accuracy, dataset keys")
+                "loss, accuracy")
     path = pj(project_dir, "%s_results.csv" % (trainer_name))
     data = []
     prev_dict = OrderedDict()
@@ -67,26 +153,26 @@ def write_training_results(results_dict, cfg, project_dir, trainer_name):
                 prev_dict[h].append( row[i] )
     N = len(data)
 
+    cfg_keys = cfg.keys()
     prev_keys = prev_dict.keys()
     new_dict = OrderedDict()
+
     for pk in prev_keys:
         new_dict[pk] = prev_dict[pk]
         if pk in results_keys:
             new_dict[pk].append( results_dict[pk] )
-        else:
+        elif pk in cfg_keys:
             break
+        else:
+            new_dict[pk] = prev_dict[pk]
+            new_dict[pk].append("")
     for rk in results_keys:
         if rk not in new_dict.keys():
             new_dict[rk] = [""] * N
             new_dict[rk].append( results_dict[rk] )
-    cfg_keys = cfg.keys()
+
     for pk in prev_keys:
-        if pk in results_keys:
-            continue
-        if pk in prev_keys:
-            new_dict[pk] = prev_dict[pk]
-        else:
-            new_dict[pk] = [""] * N
+        new_dict[pk] = prev_dict[pk]
         if pk in cfg_keys:
             new_dict[pk].append( cfg[pk] )
         else:
