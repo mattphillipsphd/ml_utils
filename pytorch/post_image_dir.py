b3c3487fd192913761853c159277116df08e08a4
matthew.phillips <matthew.phillips@lifeomic.com>
2020-01-04 14:21:37 -0500

    Created post_image_dir.py to visualize directories from tensorboard

---
+++ pytorch/post_image_dir.py
@@ -0,0 +1,53 @@
+"""
+Takes directory of images and posts them to tensorboard
+"""
+
+import argparse
+import numpy as np
+import os
+import shutil
+import sys
+from PIL import Image
+
+import torch
+import torchvision as tv
+
+from tensorboardX import SummaryWriter
+
+from pytorch.pyt_utils.utils import get_summary_writer
+
+pe = os.path.exists
+pj = os.path.join
+HOME = os.path.expanduser("~")
+
+
+def main(cfg):
+    input_dir = os.path.abspath( cfg["input_dir"] )
+    tboard_supdir = os.path.abspath( cfg["tboard_supdir"] )
+    tboard_dir = pj(tboard_supdir, os.path.basename(input_dir))
+    if pe(tboard_dir):
+        shutil.rmtree(tboard_dir)
+    os.makedirs(tboard_dir)
+    print("Created tensorboard logdir %s" % tboard_dir)
+
+    writer = get_summary_writer(tboard_dir)
+    T = tv.transforms.ToTensor()
+
+    images = [f for f in os.listdir(input_dir) if f.endswith(cfg["image_ext"])]
+    for i,img_name in enumerate(images):
+        img = Image.open( pj(input_dir, img_name) )
+        writer.add_image(img_name, T(img), i)
+    print("%d images added." % len(images))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input-dir", type=str, required=True)
+    parser.add_argument("-e", "--image-ext", type=str, default="")
+    parser.add_argument("-o", "--tboard-supdir", type=str, 
+            default=pj(HOME, "tensorboard"))
+    parser.add_argument("-f", "--force-overwrite", action="store_true",
+            help="If set, preexisting output directory will be deleted")
+    cfg = vars( parser.parse_args() )
+    main(cfg)
+
